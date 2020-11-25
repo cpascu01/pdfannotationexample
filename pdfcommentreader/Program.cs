28ba04c9aa8ef6405c34d5a8641c6eeb546b565b
Chris Pascual <christopher.pascual@gmail.com>
2020-11-25 18:44:05 -0500

    initial commit

---
+++ pdfcommentreader/Program.cs
@@ -0,0 +1,25 @@
+﻿using iText.Kernel.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdfcommentreader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PdfReader reader = new PdfReader(@"doc.pdf");
+            PdfDocument doc = new PdfDocument(reader);
+            var notes = doc.GetPage(1).GetAnnotations();
+
+            foreach (var note in notes)
+            {
+                Console.WriteLine(note.GetContents());
+            }
+            Console.ReadKey();
+        }
+    }
+}
